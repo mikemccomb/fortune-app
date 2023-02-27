253cd96614e7a55f11d9731120a9866f89831b4d
mikemccomb <mikemccomb@gmail.com>
2023-02-27 13:01:09 -0600

    random fortune feature

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -1,2 +1,14 @@
 class MyExamplesController < ApplicationController
+  def random_fortune
+    array = [0, 1, 2]
+    array = array.shuffle
+    fortune = array[0]
+    if fortune == 0
+      render json: { fortune: "Uh oh..." }
+    elsif fortune == 1
+      render json: { fortune: "Keep on keeping on" }
+    else
+      render json: { fortune: "Look under your seat..." }
+    end
+  end
 end
